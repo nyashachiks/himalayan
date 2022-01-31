552bd4e2c3f9cdb949cde36f8d657f167ea3ada8
Nyasha Chikwanda <nyashachiks@gmail.com>
2022-01-31 17:22:55 +0200

    Created templates main.html, store.html, product.html, cart.html and checkout.html as well as blank views and url patterns and setup all necessary config to run the Django project before adding Python and Django code

---
+++ everest/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'everest'
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('product', views.product, name='product'),
+    path('cart', views.cart, name='cart'),
+    path('checkout', views.checkout, name='checkout')
+    ]
